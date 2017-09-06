bc9e96ce4afebf280cc1156df2f18260d5aa0d3c
ben7ny <benjason777@gmail.com>
2017-09-06 01:40:51 -0400

    add scroll animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,32 @@
+var pointsArray = document.getElementsByClassName('point');
 
-function revealPoint (points) {
-
-  for (var i = 0; i < points.length; i++ ){
+var animatePoints = function(points) {
+  function revealPoint(i) {
     points[i].style.opacity = 1;
     points[i].style.transform = "scaleX(1) translateY(0)";
     points[i].style.msTransform = "scaleX(1) translateY(0)";
     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
 
+  for (var i = 0; i < points.length; i++ ){
+    revealPoint(i);
   };
 }
-var points = document.getElementsByClassName('point');
 
-revealPoint(points);
+// animatePoints();
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  window.addEventListener('scroll', function(event) {
+    // console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
